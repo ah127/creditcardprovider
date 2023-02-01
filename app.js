c7a66cddffcad63373607e4a075c6d551fa3c570
Kumar Yogi <kumnaaryongi904.ky@gmail.com>
2023-02-01 16:13:51 +0000

    added get and create controllers als allowed the cors

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const creditCardRoutes = require('./routes/creditcard');
 
-// const errorController = require('./controllers/error');
+app.use(bodyParser.json()) //application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next();
+})
+
 app.use(creditCardRoutes);
+
 app.listen(3000,()=>{
     console.log("app is running");
 })
--- routes/creditcard.js
+++ routes/creditcard.js
@@ -5,4 +5,6 @@ const creditCardController = require('../controllers/creditcard');
 
 router.get('/cards', creditCardController.getCreditCards);
 
+router.post('/cards', creditCardController.createCreditCards);
+
 module.exports = router;
